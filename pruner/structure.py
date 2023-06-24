5bcad967f9bea7313ddd543f57160908630f95cd
mba <zcl.neber@hotmail.com>
2023-06-24 23:38:55 +0800

    add mvit and test file

--- pruner/structure.py
+++ pruner/structure.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.utils.prune as prune
 
+
 class Pruner:
     def __init__(self, model) -> None:
         pass
 
 
-def structure_conv(conv: nn.Module, index: list, dim: int)->nn.Module:
+def structure_conv(conv: nn.Module, index: list, dim: int) -> nn.Module:
     """
     Prune the Convolution Layer:
     return a new conv layer with pruned weight
@@ -18,12 +19,25 @@ def structure_conv(conv: nn.Module, index: list, dim: int)->nn.Module:
     :return new_conv: nn.Conv2d
     """
     assert dim in [0, 1], "dim must be 0 or 1"
+    if len(index) == 0:
+        return conv
     weight_shape = list(conv.weight.shape)
     # invert the index
     saved_index = [i for i in range(weight_shape[dim]) if i not in index]
     weight_shape[dim] -= len(index)
-    new_conv = nn.Conv2d(weight_shape[1], weight_shape[0], weight_shape[2:], stride=conv.stride, padding=conv.padding, dilation=conv.dilation, groups=conv.groups, bias=conv.bias is not None)
-    new_conv.weight = nn.Parameter(torch.index_select(conv.weight, dim, torch.tensor(saved_index)))
+    new_conv = nn.Conv2d(
+        weight_shape[1],
+        weight_shape[0],
+        weight_shape[2:],
+        stride=conv.stride,
+        padding=conv.padding,
+        dilation=conv.dilation,
+        groups=conv.groups,
+        bias=conv.bias is not None,
+    )
+    new_conv.weight = nn.Parameter(
+        torch.index_select(conv.weight, dim, torch.tensor(saved_index))
+    )
     if conv.bias is not None:
         if dim == 0:
             # new_conv.bias = nn.Parameter(torch.index_select(conv.bias, dim, torch.tensor(saved_index)))
@@ -32,7 +46,8 @@ def structure_conv(conv: nn.Module, index: list, dim: int)->nn.Module:
             new_conv.bias.data = conv.bias.data
     return new_conv
 
-def structure_fc(fc: nn.Module, index: list, dim: int)->nn.Module:
+
+def structure_fc(fc: nn.Module, index: list, dim: int) -> nn.Module:
     """
     Prune the Fully Connected Layer:
     return a new fc layer with pruned weight
@@ -42,19 +57,24 @@ def structure_fc(fc: nn.Module, index: list, dim: int)->nn.Module:
     :return new_fc: nn.Linear
     """
     assert dim in [0, 1], "dim must be 0 or 1"
+    if len(index) == 0:
+        return fc
     weight_shape = list(fc.weight.shape)
     # invert the index
     saved_index = [i for i in range(weight_shape[dim]) if i not in index]
     weight_shape[dim] -= len(index)
     new_fc = nn.Linear(weight_shape[1], weight_shape[0], bias=fc.bias is not None)
-    new_fc.weight.data = nn.Parameter(torch.index_select(fc.weight, dim, torch.tensor(saved_index)))
+    new_fc.weight.data = nn.Parameter(
+        torch.index_select(fc.weight, dim, torch.tensor(saved_index))
+    )
     if fc.bias is not None:
         if dim == 0:
-            #new_fc.bias = nn.Parameter(torch.index_select(fc.bias, dim, torch.tensor(saved_index)))
+            # new_fc.bias = nn.Parameter(torch.index_select(fc.bias, dim, torch.tensor(saved_index)))
             new_fc.bias.data = fc.bias[saved_index]
         else:
             new_fc.bias.data = fc.bias.data
     return new_fc
+
 
 def structure_bn(bn: nn.Module, index):
     """
@@ -64,23 +84,26 @@ def structure_bn(bn: nn.Module, index):
     :param index: list of index to be pruned
     :return new_bn: nn.BatchNorm2d
     """
+    if len(index) == 0:
+        return bn
     weight_shape = list(bn.weight.shape)
     saved_index = [i for i in range(weight_shape[0]) if i not in index]
     weight_shape[0] -= len(index)
-    new_bn = nn.BatchNorm2d(weight_shape[0], bn.eps, bn.momentum, bn.affine, bn.track_running_stats)
+    new_bn = nn.BatchNorm2d(
+        weight_shape[0], bn.eps, bn.momentum, bn.affine, bn.track_running_stats
+    )
     new_bn.weight.data = bn.weight.data[saved_index]
     if bn.bias is not None:
         new_bn.bias.data = bn.bias.data[saved_index]
     return new_bn
 
 
-
 if __name__ == "__main__":
     conv = nn.Conv2d(3, 6, 3, bias=False)
     print(conv.weight.shape)
-    print(conv.weight[2,0,0,0])
-    print(conv.weight[3,0,0,0])
+    print(conv.weight[2, 0, 0, 0])
+    print(conv.weight[3, 0, 0, 0])
     new_conv = structure_conv(conv, [0, 1], 0)
     print(new_conv.weight.shape)
-    print(new_conv.weight[0,0,0,0])
-    print(new_conv.weight[1,0,0,0])
+    print(new_conv.weight[0, 0, 0, 0])
+    print(new_conv.weight[1, 0, 0, 0])
--- pruner/utils.py
+++ pruner/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from typing import Sequence
 from structure import *
 
+
 def get_sparsity(model, amount):
     """
     Get the sparsity dict for a model.
@@ -17,25 +18,33 @@ def get_sparsity(model, amount):
             in_ch = m.in_channels
             out_ch = m.out_channels
             strides = m.stride
-            featio = out_ch/strides[0]/strides[1]/in_ch/(torch.sum(m.weight.data).item()+1e-8)
+            featio = (
+                out_ch
+                / strides[0]
+                / strides[1]
+                / in_ch
+                / (torch.sum(m.weight.data).item() + 1e-8)
+            )
         elif isinstance(m, nn.Linear):
             # print("Linear layer", k)
             in_ch = m.in_features
             out_ch = m.out_features
-            featio = out_ch/in_ch/(torch.sum(m.weight.data).item()+1e-8)
-        else: 
+            featio = out_ch / in_ch / (torch.sum(m.weight.data).item() + 1e-8)
+        else:
             continue
-        sparsity_dict[k] = 1/featio
+        sparsity_dict[k] = 1 / featio
         sum += sparsity_dict[k]
     for k in sparsity_dict.keys():
-        sparsity_dict[k] = amount*(1-sparsity_dict[k]/sum)
+        sparsity_dict[k] = amount * (1 - sparsity_dict[k] / sum)
     return sparsity_dict
 
-#def prune_model(model, sparsity_dict):
+
+# def prune_model(model, sparsity_dict):
 #    """
 #    Prune the model according to the sparsity dict.
 #    """
 #    pass
+
 
 def __list_to_dict(list: Sequence[str]) -> dict:
     """
@@ -48,14 +57,21 @@ def __list_to_dict(list: Sequence[str]) -> dict:
             dict[k1][k2] = 0
     return dict
 
-    
 
-def get_dep_dcit(model, example_input=torch.randn(2,3,32,32), type_list=[nn.Conv2d, nn.Linear, nn.BatchNorm2d], ignore_list=[]):
+def get_dep_dcit(
+    model,
+    example_input=torch.randn(2, 3, 224, 224),
+    type_list=[nn.Conv2d, nn.Linear, nn.BatchNorm2d],
+    ignore_list=[],
+    debug=False,
+):
     """
     Get the dependency dict for a model.
     """
     # get the white list and module dict
-    white_list = ['output']
+    if debug:
+        print("Get white list and module dict...")
+    white_list = ["output"]
     module_list = {}
     for k, m in model.named_modules():
         if not isinstance(m, tuple(type_list)):
@@ -64,40 +80,60 @@ def get_dep_dcit(model, example_input=torch.randn(2,3,32,32), type_list=[nn.Conv
             continue
         white_list.append(k)
         module_list[k] = m
-    
+
     # get the dependency dict
+    if debug:
+        print("Get dependency dict...")
     dep_dict = __list_to_dict(white_list)
-    
+
     # get the name dict for id address to key
+    if debug:
+        print("Get name dict...")
     name_dict = {}
     for k, m in model.named_modules():
         name_dict[id(m)] = k
 
     # get module forward and backward dict
     forward_dict, backward_dict = {}, {}
+
     def __forward_hook_callback(module, input, output):
         forward_dict[name_dict[id(module)]] = {}
-        forward_dict[name_dict[id(module)]]['output'] = output
-        forward_dict[name_dict[id(module)]]['input'] = input[0]
+        forward_dict[name_dict[id(module)]]["output"] = output
+        forward_dict[name_dict[id(module)]]["input"] = input[0]
+
     def __backward_hook_callback(module, grad_input, grad_output):
         backward_dict[name_dict[id(module)]] = {}
-        backward_dict[name_dict[id(module)]]['output'] = grad_output[0]
-        backward_dict[name_dict[id(module)]]['input'] = grad_input
+        backward_dict[name_dict[id(module)]]["output"] = grad_output[0]
+        backward_dict[name_dict[id(module)]]["input"] = grad_input
+
     hooks = []
+    if debug:
+        print("Get forward and backward dict...")
     for k, m in module_list.items():
         hooks.append(m.register_forward_hook(__forward_hook_callback))
         hooks.append(m.register_backward_hook(__backward_hook_callback))
     out = model(example_input)
-    out.sum().backward()
+    try:
+        out.sum().backward()
+    except:
+        outs = out[-1].sum()
+        # outs = 0
+        # for o in out:
+        #     outs += o.sum()
+        outs.backward()
 
     # fill the dependency dict
     def __check_id(g):
         for k, m in forward_dict.items():
-            if m['output'].grad_fn == g:
+            if m["output"].grad_fn == g:
                 return k
-        return 'none'
-    def __fill_dep(parent, g, level=0): # TODO level: useless params
-        if g == None: return
+        return "none"
+
+    checked_list = []
+
+    def __fill_dep(parent, g, level=0):  # TODO level: useless params
+        if g == None:
+            return
         # check current node name
         # if g.__class__.__name__ == 'AddBackward0':
         #     if parent[-4:] == '.add':
@@ -106,28 +142,57 @@ def get_dep_dcit(model, example_input=torch.randn(2,3,32,32), type_list=[nn.Conv
         # else:
         #     id = __check_id(g)
         id = __check_id(g)
-        if id != 'none':
+        if id != "none":
             dep_dict[parent][id] = 1
             dep_dict[id][parent] = -1
         else:
             # print('Warning: {} not in white list'.format(g.__class__.__name__))
             id = parent
+        checked_list.append(g)
         for subg in g.next_functions:
-            __fill_dep(id, subg[0], level+1)
-    __fill_dep('output', out.grad_fn, 0)
+            if subg[0] in checked_list:
+                continue
+            __fill_dep(id, subg[0], level + 1)
+
+    # def __fill_dep(parent, g， level=0):
+    #     stack = [(parent, g)]
+    #     while stack:
+    #         curr_parent, curr_g = stack.pop()
+    #         if curr_g is None:
+    #             continue
+    #         id = __check_id(curr_g)
+    #         if id != "none":
+    #             dep_dict[curr_parent][id] = 1
+    #             dep_dict[id][curr_parent] = -1
+    #         else:
+    #             id = curr_parent
+    #         for subg in curr_g.next_functions:
+    #             stack.append((id, subg[0]))
+
+    if debug:
+        print("Fill dependency dict...")
+    try:
+        __fill_dep("output", out.grad_fn, 0)
+    except:
+        __fill_dep("output", outs.grad_fn, 0)
     for hook in hooks:
         hook.remove()
+    if debug:
+        print("get dependency dict finished.")
     return dep_dict, module_list, name_dict
 
-def get_group_list(dep_dict, module_list, name_dict):
+
+def get_group_list(dep_dict, module_list, name_dict, debug=False):
     related_dict = {}
+    if debug:
+        print("Get related dict...")
     for k1, v1 in dep_dict.items():
-        related_dict[k1] = {'in':[], 'out':[]}
+        related_dict[k1] = {"in": [], "out": []}
         for k2, v2 in v1.items():
             if v2 == 1:
-                related_dict[k1]['in'].append(k2)
+                related_dict[k1]["in"].append(k2)
             elif v2 == -1:
-                related_dict[k1]['out'].append(k2)
+                related_dict[k1]["out"].append(k2)
             else:
                 continue
 
@@ -139,7 +204,7 @@ def get_group_list(dep_dict, module_list, name_dict):
     def __add_layer(m_list, layer_type):
         tmp_list = []
         for i in m_list:
-            if i == 'output':
+            if i == "output":
                 continue
             if isinstance(module_list[i], nn.BatchNorm2d):
                 tmp_list += __add_layer(related_dict[i][layer_type], layer_type)
@@ -147,9 +212,10 @@ def get_group_list(dep_dict, module_list, name_dict):
                 tmp_list.append(i)
         return tmp_list
 
-
+    if debug:
+        print("add all input layer to list...")
     for k, v in related_dict.items():
-        layer = __add_layer(v['in'], 'in')
+        layer = __add_layer(v["in"], "in")
         layers.append(layer)
         # layer_next = __add_layer(v['out'], 'out')
         # layers_next.append(layer_next)
@@ -178,35 +244,52 @@ def get_group_list(dep_dict, module_list, name_dict):
             if not merged:
                 merged_data.append(sublist)
         return list(filter(None, merged_data))
-    
+
+    if debug:
+        print("get merged data...")
     layers = __get_merged_data(layers)
     # layers_next = __get_merged_data(layers_next)
 
+    if debug:
+        print("get group list...")
     for i in layers:
-        group = {'modules': [], 'next': []}
+        group = {"modules": [], "next": []}
         for j in i:
-            group['modules'].append(j)
-            group['next'].extend(related_dict[j]['out'])
-            for k in related_dict[j]['out']:
-                if k == 'output':
+            group["modules"].append(j)
+            group["next"].extend(related_dict[j]["out"])
+            for k in related_dict[j]["out"]:
+                if k == "output":
                     continue
-                if isinstance(module_list[k], nn.BatchNorm2d):
-                    group['next'].extend(related_dict[k]['out'])
-        group['next'] = list(set(group['next']))
+                if isinstance(module_list[k], (nn.BatchNorm2d, nn.LayerNorm)):
+                    group["next"].extend(related_dict[k]["out"])
+        group["next"] = list(set(group["next"]))
         group_list.append(group)
 
+    if debug:
+        print("get group list finished.")
     return group_list
 
 
-def prune_model(model, ratio=0.5, prune_type='random'):
-    dep_dict, module_list, name_dict = get_dep_dcit(model)
-    group_list = get_group_list(dep_dict, module_list, name_dict)
-    for group in group_list:
-       print("modules:", group['modules'])
-       print("next_modules:", group['next'])
+def prune_model(
+    model,
+    ratio=0.5,
+    prune_type="random",
+    example_input=torch.randn(2, 3, 32, 32),
+    ignore_list=[[], []],
+    debug=False,
+):
+    if debug:
+        print("start getting dep dict")
+    dep_dict, module_list, name_dict = get_dep_dcit(model, example_input, debug=debug)
+    if debug:
+        print("start getting group list")
+    group_list = get_group_list(dep_dict, module_list, name_dict, debug=debug)
+    # for group in group_list:
+    #     print("modules:", group["modules"])
+    #     print("next_modules:", group["next"])
 
     def __set_module(name, new_module):
-        name_list = name.split('.')
+        name_list = name.split(".")
         module = model
         for i in name_list[:-1]:
             if i.isdigit():
@@ -214,21 +297,46 @@ def prune_model(model, ratio=0.5, prune_type='random'):
             else:
                 module = getattr(module, i)
         setattr(module, name_list[-1], new_module)
-        
 
+    if debug:
+        print("start pruning")
     for i in group_list:
-        #if i['next'] == ['output']: # TODO: delete this and add in the get_group_list
+        # if i['next'] == ['output']: # TODO: delete this and add in the get_group_list
         #    continue
-        if isinstance(module_list[i['modules'][0]], nn.Conv2d):
-            out_ch = module_list[i['modules'][0]].out_channels
+        if isinstance(module_list[i["modules"][0]], nn.Conv2d):
+            out_ch = module_list[i["modules"][0]].out_channels
         else:
-            out_ch = module_list[i['modules'][0]].out_features
+            out_ch = module_list[i["modules"][0]].out_features
 
-        if prune_type == 'random':
-            prune_num = int(out_ch * ratio)
+        # calculate round_to
+        round_to = 1
+        for j in i["next"]:
+            if j == "output":
+                continue
+            if isinstance(module_list[j], nn.Conv2d):
+                round_to = module_list[j].in_channels
+                break
+
+        if prune_type == "random":
+            prune_num2 = int(out_ch * ratio)
+            prune_num = int(round(out_ch * ratio / round_to) * round_to)
             prune_idx = torch.randperm(out_ch)[:prune_num]
-        
-        for j in i['modules']:
+
+        if debug:
+            print(
+                "round_to:",
+                round_to,
+                "prune_num:",
+                prune_num,
+                "prune_num2:",
+                prune_num2,
+            )
+
+        for j in i["modules"]:
+            if debug:
+                print("pruning modules:", j, module_list[j])
+            if module_list[j] in ignore_list[0]:
+                continue
             if isinstance(module_list[j], nn.Conv2d):
                 pruned_module = structure_conv(module_list[j], prune_idx, 0)
             elif isinstance(module_list[j], nn.Linear):
@@ -236,8 +344,12 @@ def prune_model(model, ratio=0.5, prune_type='random'):
             module_list[j] = pruned_module
             __set_module(j, pruned_module)
 
-        for j in i['next']:
-            if j == 'output':
+        for j in i["next"]:
+            if j == "output":
+                continue
+            if debug:
+                print("pruning next:", j, module_list[j])
+            if module_list[j] in ignore_list[1]:
                 continue
             if isinstance(module_list[j], nn.Conv2d):
                 pruned_module = structure_conv(module_list[j], prune_idx, 1)
@@ -246,44 +358,78 @@ def prune_model(model, ratio=0.5, prune_type='random'):
             elif isinstance(module_list[j], nn.BatchNorm2d):
                 pruned_module = structure_bn(module_list[j], prune_idx)
             module_list[j] = pruned_module
-            __set_module(j, pruned_module) #structure_conv(module_list[j], prune_idx, 1))
-        
-    for k, m in model.named_modules():
-        if isinstance(m, (nn.Conv2d, nn.Linear, nn.BatchNorm2d)):
-            print(k, m)
+            __set_module(
+                j, pruned_module
+            )  # structure_conv(module_list[j], prune_idx, 1))
 
-    print(model(torch.randn(2,3,32,32)).shape)
-
+    # for k, m in model.named_modules():
+    #     if isinstance(m, (nn.Conv2d, nn.Linear, nn.BatchNorm2d)):
+    #         print(k, m)
+    if debug:
+        print("finish pruning")
     return model
-        
+
 
 def __test_get_dep_dict():
     from torchvision.models import resnet18
+
     # model = conv5()
     model = resnet18()
     dep_dict, module_list, name_dict = get_dep_dcit(model)
     # convert dict to pd
     import pandas as pd
+
     dep_df = pd.DataFrame(dep_dict)
-    dep_df.to_csv('~/Downloads/dep.csv')
+    dep_df.to_csv("~/Downloads/dep.csv")
+
 
 def __test_get_group_list():
     from torchvision.models import resnet18
+
     # model = conv5()
     model = resnet18()
     dep_dict, module_list, name_dict = get_dep_dcit(model)
     group_list = get_group_list(dep_dict, module_list, name_dict)
     for group in group_list:
-       print("modules:", group['modules'])
-       print("next_modules:", group['next'])
+        print("modules:", group["modules"])
+        print("next_modules:", group["next"])
+
 
 def __test_pruning_model():
     from torchvision.models import resnet18
-    # model = conv5()
-    model = resnet18()
-    prune_model(model)
+    from thop import profile
+    from mvit import mobilevit_s, Attention
 
-if __name__ == '__main__':
+    # model = resnet18()
+    model = mobilevit_s(320)
+    example_input = torch.randn(2, 3, 320, 320)
+    macs, params = profile(model, inputs=(example_input,))
+    print("macs:", macs, "params:", params)
+
+    il = [[], []]  # ignore_list
+    for k, m in model.named_modules():
+        if isinstance(m, Attention):
+            il[0].append(m.to_qkv)
+            if m.to_out[0]:
+                il[1].append(m.to_out[0])
+    model_new = prune_model(
+        model, ratio=0.5, example_input=example_input, ignore_list=il, debug=True
+    )
+    # for k, m in model_new.named_modules():
+    #     if isinstance(m, (nn.Conv2d, nn.Linear, nn.BatchNorm2d, nn.LayerNorm)):
+    #         print(k, m)
+
+    output = model_new(example_input)
+    try:
+        print(output.shape)
+    except:
+        for i in output:
+            print(i.shape)
+    macs, params = profile(model_new, inputs=(example_input,))
+    print("macs:", macs, "params:", params)
+
+
+if __name__ == "__main__":
     # __test_get_dep_dict()
     # __test_get_group_list()
     __test_pruning_model()
