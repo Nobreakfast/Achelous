b61dc79b24a144a68b6329aa5622fd4bc5e3f60f
Nobreakfast <zcl.neber@hotmail.com>
2023-09-08 15:52:22 +0800

    update test results

---
+++ test_fps.sh
@@ -0,0 +1,8 @@
+python train_prune_fps.py --backbone mv --neck gdf --phi S2 --pa erk --pm 0.35
+python train_prune_fps.py --backbone mv --neck cdf --phi S2 --pa erk --pm 0.48
+python train_prune_fps.py --backbone pf --neck cdf --phi S2 --pa erk --pm 0.62
+python train_prune_fps.py --backbone pf --neck cdf --phi S2 --pa erk --pm 0.55
+python train_prune_fps.py --backbone pf --neck gdf --phi S2 --pa erk --pm 0.52
+python train_prune_fps.py --backbone pf --neck gdf --phi S2 --pa erk --pm 0.45
+python train_prune_fps.py --backbone ef --neck gdf --phi S2 --pa erk --pm 0.36
+python train_prune_fps.py --backbone ef --neck cdf --phi S2 --pa erk --pm 0.50
